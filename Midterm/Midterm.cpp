798ec38adbd2a23727cb43e9d35468934f19beda
xem <kmoniz@student.fullsail.edu>
2022-05-16 21:30:19 -0400

    Add empty project and gitignore

---
+++ Midterm/Midterm.cpp
@@ -0,0 +1,9 @@
+// Midterm.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Kevin Moniz
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
