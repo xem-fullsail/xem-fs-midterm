b9b21fbf1cdacfa9908e6be6b634e5bcfcc56927
xem-fullsail <kmoniz@student.fullsail.edu>
2022-05-18 20:07:34 -0400

    Refactor Midterm. Main now asks the user a random question and checks for correct answer.

--- Midterm/Midterm.cpp
+++ Midterm/Midterm.cpp
@@ -1,7 +1,8 @@
 // Midterm.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Kevin Moniz
 
-#include "Midterm.h"
+#include <iostream>
+#include <string>
 #include "QAPair.h"
 
 int main() {
@@ -11,7 +12,19 @@ int main() {
     questions[1] = QAPair("What color is grass?", "Green");
     questions[2] = QAPair("What is mom spelled backwards?", "mom");
     questions[3] = QAPair("Marvel or DC", "I am Batman.");
+    
+    // Choose and ask a random question
+    srand((unsigned int)time(NULL));
+    int index = rand() % 4;
+    std::cout << questions[index].question << std::endl;
+    
+    // Get the user's response
+    std::string response;
+    std::getline(std::cin, response);
 
-    // Choose a random question
-    int random = rand();
+    // Verify the response
+    if (questions[index].IsCorrectAnswer(response, false))
+        std::cout << "\nYou are correct!";
+    else
+        std::cout << "\nSorry, the correct answer is: " << questions[index].answer;
 }
